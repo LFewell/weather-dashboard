efbe5f98ed62bc2025d99d9420d87e6c7c762a49
Lincoln Fewell <LincolnTFewell@gmail.com>
2021-09-27 18:40:06 -0400

    added local storage for cities searched, and started a return element

--- script.js
+++ script.js
@@ -10,20 +10,57 @@ let weather = {
         const {icon, description} = data.weather[0];
         const {temp, humidity} = data.main;
         const {speed} = data.wind;
-        console.log(name,icon,description,temp,humidity,speed,);
+        // const {uvi} = data.current.uvi;
+        console.log(name,icon,description,temp,humidity,speed);
         document.querySelector("#city").innerText = "Weather in " + name;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
         document.querySelector("#description").innerText = description;
         document.querySelector("#temp").innerText = "Temp: " + temp + "℉";
         document.querySelector("#wind").innerText = "Wind: " + speed + "mph";
         document.querySelector("#humidity").innerText = "Humidity: " + humidity + "%";
-        document.querySelector("#UV").innerText = "UV Index: ";
+        // document.querySelector("#UV").innerText = "UV Index: " + uvi;
     },
     search: function () {
         this.fetchWeather(document.querySelector(".validate").value);
-    }
+    },
 };
+
+let pastCityArray = []
+let savedCityArray = []
+
+function storeCity () {
+    pastCityArray.push(document.querySelector(".validate").value)
+    localStorage.setItem("pastCityArray", JSON.stringify(pastCityArray));
+}
+
+
+function cityButton () {
+    savedCityArray = JSON.parse(localStorage.getItem("pastCityArray")) || "";
+    console.log(savedCityArray);
+    populate = document.querySelector("#searched-container")
+    newBtn = document.createElement("button");
+    btnText = document.createTextNode(savedCityArray);
+    populate.appendChild(newBtn);
+    newBtn.innerText = btnText;
+}
+
+
+
+// let forecast = {
+//     "apiKey": "440b68c93644497197bbe6ad7736992a",
+//     fetchForecast: function(fiveday) {
+//         fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + fiveday + "&units=imperial&appid=" + this.apiKey)
+//         .then((response) => response.json())
+//         .then ((data) => this.displayForecast(data)); 
+//     },
+//     displayForecast: function(data) {
+//         const {name} = data;
+//         const 
+//     }
+// }
 
 document.querySelector("#search-button").addEventListener("click", function () {
     weather.search();
+    storeCity();
+    cityButton();
 });
