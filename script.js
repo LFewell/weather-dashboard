bb7ebf364131a914ac58857743f2dfd4addc9e23
Lincoln Fewell <LincolnTFewell@gmail.com>
2021-09-27 02:50:32 -0400

    Rewrote functions for grabbing current weather, search bar now pulls up cities current weather

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
-var apiKey = "440b68c93644497197bbe6ad7736992a"
-var searchBtn = document.getElementById("search-button")
-var city = document.getElementById("search-city")
-var userCityName = ""
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+let weather = {
+    "apiKey": "440b68c93644497197bbe6ad7736992a",
+    fetchWeather: function (city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + this.apiKey)
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data));
+    },
+        displayWeather: function(data) {
+        const {name} = data;
+        const {icon, description} = data.weather[0];
+        const {temp, humidity} = data.main;
+        const {speed} = data.wind;
+        console.log(name,icon,description,temp,humidity,speed,);
+        document.querySelector("#city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector("#description").innerText = description;
+        document.querySelector("#temp").innerText = "Temp: " + temp + "℉";
+        document.querySelector("#wind").innerText = "Wind: " + speed + "mph";
+        document.querySelector("#humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector("#UV").innerText = "UV Index: ";
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".validate").value);
+    }
+};
 
-
-searchBtn.addEventListener("click", getInputValue);
-searchBtn.addEventListener("click", getCurrentWeatherApi);
-
-function getInputValue () {
-    userCityName = city.value;
-    console.log(userCityName)
-}
-
-function getCurrentWeatherApi() {
-    fetch(queryURL)
-        .then(function (response) {
-            console.log(queryURL);
-            return response.json();
-        })
-        .then(function(data) {
-            console.log(data);
-        })
-}
+document.querySelector("#search-button").addEventListener("click", function () {
+    weather.search();
+});
