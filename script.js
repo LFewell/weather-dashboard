0fc181c1fccf0ee7904558028e80c3d526f17f70
Lincoln Fewell <LincolnTFewell@gmail.com>
2021-09-26 23:59:09 -0400

    Added the rest of the weather cards, started writing functionality for the API

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 var apiKey = "440b68c93644497197bbe6ad7736992a"
 var searchBtn = document.getElementById("search-button")
-var city = document.getElementById("search-city");
+var city = document.getElementById("search-city")
+var userCityName = ""
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 
-fetch(queryURL)
+
+searchBtn.addEventListener("click", getInputValue);
+searchBtn.addEventListener("click", getCurrentWeatherApi);
+
+function getInputValue () {
+    userCityName = city.value;
+    console.log(userCityName)
+}
+
+function getCurrentWeatherApi() {
+    fetch(queryURL)
+        .then(function (response) {
+            console.log(queryURL);
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+        })
+}
