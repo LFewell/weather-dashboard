d96653392ce891e8fd1070375806897a3ecf4637
Lincoln Fewell <LincolnTFewell@gmail.com>
2021-09-28 18:01:06 -0400

    cleaned up code and added README text

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # weather-dashboard
+
+This is a weather dashboard that will display the current weather conditions in any city.  The users searches will be saved in local storage and appear as a button below.  
+
+![Screenshot of the deployed webpage](./weatherdashboard.png)
+
+https://lfewell.github.io/weather-dashboard/
--- script.js
+++ script.js
@@ -1,66 +1,60 @@
 let weather = {
-    "apiKey": "440b68c93644497197bbe6ad7736992a",
-    fetchWeather: function (city) {
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + this.apiKey)
-        .then((response) => response.json())
-        .then((data) => this.displayWeather(data));
-    },
-        displayWeather: function(data) {
-        const {name} = data;
-        const {icon, description} = data.weather[0];
-        const {temp, humidity} = data.main;
-        const {speed} = data.wind;
-        // const {uvi} = data.current.uvi;
-        console.log(name,icon,description,temp,humidity,speed);
-        document.querySelector("#city").innerText = "Weather in " + name;
-        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
-        document.querySelector("#description").innerText = description;
-        document.querySelector("#temp").innerText = "Temp: " + temp + "℉";
-        document.querySelector("#wind").innerText = "Wind: " + speed + "mph";
-        document.querySelector("#humidity").innerText = "Humidity: " + humidity + "%";
-        // document.querySelector("#UV").innerText = "UV Index: " + uvi;
-    },
-    search: function () {
-        this.fetchWeather(document.querySelector(".validate").value);
-    },
+  apiKey: "440b68c93644497197bbe6ad7736992a",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=imperial&appid=" +
+        this.apiKey
+    )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    // const {uvi} = data.current.uvi;
+    console.log(name, icon, description, temp, humidity, speed);
+    document.querySelector("#city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector("#description").innerText = description;
+    document.querySelector("#temp").innerText = "Temp: " + temp + "℉";
+    document.querySelector("#wind").innerText = "Wind: " + speed + "mph";
+    document.querySelector("#humidity").innerText =
+      "Humidity: " + humidity + "%";
+    // document.querySelector("#UV").innerText = "UV Index: " + uvi;
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".validate").value);
+  },
 };
 
-let pastCityArray = []
-let savedCityArray = []
+let pastCityArray = [];
+let savedCityArray = [];
 
-function storeCity () {
-    pastCityArray.push(document.querySelector(".validate").value)
-    localStorage.setItem("pastCityArray", JSON.stringify(pastCityArray));
+function storeCity() {
+  pastCityArray.push(document.querySelector(".validate").value);
+  localStorage.setItem("pastCityArray", JSON.stringify(pastCityArray));
 }
 
-
-function cityButton () {
-    savedCityArray = JSON.parse(localStorage.getItem("pastCityArray")) || "";
-    console.log(savedCityArray);
-    populate = document.querySelector("#searched-container")
-    newBtn = document.createElement("button");
-    btnText = document.createTextNode(savedCityArray);
-    populate.appendChild(newBtn);
-    newBtn.innerText = btnText;
+function cityButton() {
+  savedCityArray = JSON.parse(localStorage.getItem("pastCityArray")) || "";
+  console.log(savedCityArray);
+  populate = document.querySelector("#searched-container");
+  newBtn = document.createElement("button");
+  btnText = document.createTextNode(savedCityArray);
+  populate.appendChild(newBtn);
+  newBtn.innerText = document.querySelector(".validate").value;
 }
 
-
-
-// let forecast = {
-//     "apiKey": "440b68c93644497197bbe6ad7736992a",
-//     fetchForecast: function(fiveday) {
-//         fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + fiveday + "&units=imperial&appid=" + this.apiKey)
-//         .then((response) => response.json())
-//         .then ((data) => this.displayForecast(data)); 
-//     },
-//     displayForecast: function(data) {
-//         const {name} = data;
-//         const 
-//     }
-// }
-
+function pullStorage () {
+    JSON.parse(localStorage.getItem("pastCityArray")) || "";
+}
 document.querySelector("#search-button").addEventListener("click", function () {
-    weather.search();
-    storeCity();
-    cityButton();
+  weather.search();
+  storeCity();
+  cityButton();
 });
